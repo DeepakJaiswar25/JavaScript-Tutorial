c5149274d6141b613215ac03510ea865cb15e86b
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-06-17 22:28:24 -0500

    Basics of arrays in JS

---
+++ 02_Basics/arrays1.js
@@ -0,0 +1,37 @@
+const arr= [1,2,3,4]
+// console.log(arr)
+
+const arr2= new Array(5,6,7,8)
+// console.log(arr2);
+
+
+const  myarr=[1,3,2,9,6,5]
+// console.log(myarr[4]);
+
+// console.log(myarr.length)
+
+// console.log(myarr.shift())
+// console.log(myarr)
+
+// console.log(myarr.unshift(8))
+// console.log(myarr)
+
+// myarr.push(12)
+// myarr.push(81)
+// console.log(myarr)
+// myarr.pop();
+// myarr.pop();
+// console.log(myarr)
+
+// console.log("A",myarr)
+// console.log(myarr.slice(1,3))
+// console.log("B",myarr)
+// console.log(myarr.splice(1,3))
+// console.log("C",myarr)
+
+const newArray=myarr.join()
+// console.log(newArray)
+// console.log(typeof newArray)
+
+console.log(myarr.includes(3))
+console.log(myarr.indexOf(3))
