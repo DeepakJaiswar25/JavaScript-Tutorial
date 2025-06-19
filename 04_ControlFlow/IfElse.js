1eaf5b2571bfb026aacfc502cdc901f3c194345c
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-06-19 14:12:17 -0500

    learn about if else in JS

---
+++ 04_ControlFlow/IfElse.js
@@ -0,0 +1,51 @@
+
+
+// if(2==2){
+//     console.log("Hello");
+// }
+// else{
+//     console.log("Byee ")
+// }
+
+// const userLoggedIn= true
+// const temp=31
+// if (temp<40){
+//     console.log("temp less than 40")
+// }
+// else{
+//     console.log("temp greater than 40")
+// }
+
+// const score=200
+// if(score>100){
+//     let power ="fly"
+//     console.log(`My superpower is ${power}`)
+// }
+// console.log(`My superpower is${power}`)
+
+const balance =1000
+// if(balance<1220) console.log("Balance less than 1220"),console.log("Hello")  not appreciated 
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<700){
+//     console.log("less than 700");
+// }
+// else if(balance<900){
+//     console.log("less than 900");
+// }
+// else if(balance<1200){
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn= true
+const debitcard=true
+const loginfromgmail=true
+const loginfromemail=false
+if(userLoggedIn && debitcard){
+    console.log("Allowed to purchase course")
+}
+if(loginfromemail || loginfromgmail){
+    console.log("Logged In")
+}
