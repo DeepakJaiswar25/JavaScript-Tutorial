11b011bcae378a3c5f2534bd57ab155e17f4dff5
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-06-19 14:13:16 -0500

    learn about Switch in JS

---
+++ 04_ControlFlow/Switch.js
@@ -0,0 +1,48 @@
+
+
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+
+    default:
+        console.log("Invalid Input")
+        break;
+}
+const months="jan"
+
+switch (months) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("February")
+        break;
+    case "mar":
+        console.log("March")
+        break;
+    case "apr":
+        console.log("April")
+        break;
+    case "may":
+        console.log("May")
+        break;
+
+    default:
+        console.log("Invalid Input")
+        break;
+}
