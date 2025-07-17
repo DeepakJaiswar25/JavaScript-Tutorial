062513d03b223bf450bbdb0de0223c8a85acca31
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-07-16 23:37:27 -0500

    Inheritance in

---
+++ 11_AdvanceJS2/inheritance.js
@@ -0,0 +1,35 @@
+class user{
+
+    constructor(username) {
+        this.username=username
+       
+    }
+   logMe(){
+    console.log(`Username: ${this.username}`)
+   }
+
+
+}
+
+class teacher extends user{
+constructor(username,email,password){
+    super(username)
+    this.email=email
+    this.password=password
+}
+
+addcourse(){
+    console.log(`Course added by ${this.username}`)
+}
+
+}
+
+const chai= new user('chai','chai@gmail.com','chai@123')
+console.log(chai)
+const masalachai= new teacher('masalachai','masalachai@gmail.com','masalachai@123')
+console.log(masalachai)
+
+chai.logMe()
+masalachai.addcourse()
+masalachai.logMe()
+// chai.addcourse()
