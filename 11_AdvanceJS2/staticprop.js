4c2e320c691d71552cab72bd2d624dacf582d47d
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-07-19 23:22:50 -0500

    Staticprop in JS

---
+++ 11_AdvanceJS2/staticprop.js
@@ -0,0 +1,23 @@
+class users{
+    constructor(username,email,password){
+        this.username=username
+        this.email=email
+        this.password=password
+
+    }
+
+     static logMe(){
+        console.log(`Username: ${this.username}`)
+
+    }
+
+}
+class teacher extends users{
+    constructor(username,subject){
+        super(username)
+        this.subject=subject
+    }
+}
+
+const chai=  new teacher('Deepak','DSA')
+chai.logMe()  // it gives error because static methods are accessed only by class and not by instance if it is users.logMe it will work
